4a8eeb8d5f1b17834ee0109c9dcd7ef60e75495a
caleb-git <calebgithub@yahoo.com>
2024-05-31 18:22:08 -0600

    first commit

---
+++ src/utils/data.ts
@@ -0,0 +1,24 @@
+const services = [
+    {
+        title: "Desarrollo de Software",
+        description: "Desarrollamos herramientas a la medida de los requisitos planteados por el cliente, apoyamos en brindar opciones para lograr sus objetivos",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Sistemas de apoyo a la toma de decisiones",
+        description: "IntelliView es un sistema que facilita el acceso a la información clave de la empresa, y apoyamos la obtención de la misma, buscando la simplicidad en los procesos",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Productos de Linea",
+        description: "Complementamos nuestra oferta de servicios con productos como el sistema de administración de laboratorios MLab. Y además mantenemos la capacidad de adaptarnos a nuevos entornos con desarrollos específicos para esta plataforma",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    
+    
+];
+
+export { services };
